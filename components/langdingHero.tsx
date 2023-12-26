5986c2c350fb441dde38d7c89ff325f1e40ab3b0
Dhanubalde <kedanubalde41@gmail.com>
2023-12-26 09:16:21 +0800

    send

--- components/langdingHero.tsx
+++ components/langdingHero.tsx
@@ -3,8 +3,8 @@
 
 const LangdingHero = () => {
   return (
-    <div className="text-white text-center space-y-4 p-20 sm:text-md sm:font-bold">
-      <h1 className=" text-xl">
+    <div className="text-white text-center space-y-4 p-20 sm:text-lg sm:font-bold">
+      <h1 className="">
         Expert demand a powerful service Ai 
       </h1> 
     </div>
