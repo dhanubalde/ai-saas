9d38d5042ab86f405bf038bbb994ac95c7647b02
Dhanubalde <kedanubalde41@gmail.com>
2024-01-03 15:17:08 +0800

    gd

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -22,7 +22,7 @@ const Settingpage = async () => {
       />
       <div className=" px-4 lg:px-8 space-y-4"> 
         <div className=" text-muted-foreground text-sm">
-          {isPro ? "You are currently in a Pro Plan" : "You are currnetly on a free plan " }
+          {isPro ? "You are currently in a Pro Plan" : "You are currently on a free plan " }
         </div>
         <SubscriptionButton isPro={isPro} />
       </div>
